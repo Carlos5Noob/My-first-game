7d2fb8d2dd5d544a31b2803fd1c2939be3ba0bd9
pruebaGit <prueba@gmail.com>
2024-04-26 21:17:39 +0200

    trivial error execute

--- src/elements/Main.java
+++ src/elements/Main.java
@@ -2,10 +2,9 @@ package elements;
 
 public class Main {
     public static void main(String[] args) {
-        
+
         int salud_total = 100; // salud inicial del personaje
 
-        
         Story._story_first_section();
         Story.chooseClass();
         Story._story_second_section();
@@ -18,15 +17,16 @@ public class Main {
 
         System.out.println("Salud actual del personaje: " + salud_total);
 
+        int trivial = Minigames.trivial();
 
+        salud_total -= trivial;
+
+        System.out.println("Salud actual del personaje " + salud_total);
 
         if (salud_total < 1) {
             System.out.println("GAME OVER");
             return;
         }
 
-    
-
-    
-}
+    }
 }
